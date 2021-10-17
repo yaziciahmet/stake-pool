c451138b09a01dbd8aa7282c0405f0b8484cc03c
yaziciahmet <yaziciahmet@sabanciuniv.edu>
2021-10-17 21:57:47 +0300

    initial push

---
+++ contracts/staking/TokenVesting.sol
@@ -0,0 +1,62 @@
+pragma solidity 0.7.6;
+
+import "../dependencies/openzeppelin/contracts/SafeMath.sol";
+import "../interfaces/IMultiFeeDistribution.sol";
+
+contract TokenVesting {
+    using SafeMath for uint256;
+
+    uint256 public immutable startTime;
+    uint256 public constant duration = 86400 * 365;
+    uint256 public immutable maxMintableTokens;
+    uint256 public mintedTokens;
+    IMultiFeeDistribution public minter;
+
+    struct Vest {
+        uint256 total;
+        uint256 claimed;
+    }
+
+    mapping(address => Vest) public vests;
+
+    constructor(
+        IMultiFeeDistribution _minter,
+        uint256 _maxMintable,
+        address[] memory _receivers,
+        uint256[] memory _amounts
+    ) {
+        require(_receivers.length == _amounts.length);
+        minter = _minter;
+        uint256 mintable;
+        for (uint256 i = 0; i < _receivers.length; i++) {
+            require(vests[_receivers[i]].total == 0);
+            mintable = mintable.add(_amounts[i]);
+            vests[_receivers[i]].total = _amounts[i];
+        }
+        require(mintable == _maxMintable);
+        maxMintableTokens = mintable;
+        startTime = block.timestamp;
+    }
+
+    function claimable(address _claimer) external view returns (uint256) {
+        Vest storage v = vests[_claimer];
+        uint256 elapsedTime = block.timestamp.sub(startTime);
+        if (elapsedTime > duration) elapsedTime = duration;
+        uint256 claimable = v.total.mul(elapsedTime).div(duration);
+        return claimable.sub(v.claimed);
+    }
+
+    function claim(address _receiver) external {
+        Vest storage v = vests[msg.sender];
+        uint256 elapsedTime = block.timestamp.sub(startTime);
+        if (elapsedTime > duration) elapsedTime = duration;
+        uint256 claimable = v.total.mul(elapsedTime).div(duration);
+        if (claimable > v.claimed) {
+            uint256 amount = claimable.sub(v.claimed);
+            mintedTokens = mintedTokens.add(amount);
+            require(mintedTokens <= maxMintableTokens);
+            minter.mint(_receiver, amount, false);
+            v.claimed = claimable;
+        }
+    }
+}
